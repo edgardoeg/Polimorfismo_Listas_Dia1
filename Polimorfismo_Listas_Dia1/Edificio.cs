a0f669c44f47b8e22ff921e88ed035a0ee2e8388
edgardoeg <edgardoeg@yahoo.com>
2025-02-03 18:52:36 -0600

    Add project files.

---
+++ Polimorfismo_Listas_Dia1/Edificio.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Polimorfismo_Listas_Dia1
+{
+    public class Edificio
+    {
+
+        public string Tipo {  get; set; }
+        public double Precio {  get; set; }
+
+        public Edificio(string Tipos, double Precio)
+        {
+            this.Tipo = Tipos;
+            this.Precio = Precio;
+        }
+
+        public void Vender()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo}, ha sido vendido por el precio de L.{Precio.ToString("N2")}");
+        }
+
+        public void Vender(string Vendedor)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo}, ha sido vendido por el precio de L.{Precio.ToString("N2")}, por el vendedor {Vendedor} ");
+        }
+
+        public void Vender(string Tpo, double Precio)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Edificio de tipo {Tipo}, ha sido vendido por el precio de L.{Precio.ToString("N2")}");
+        }
+
+    }
+}
---
+++ Polimorfismo_Listas_Dia1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Polimorfismo_Listas_Dia1;
+
+string? tipo, opcion;
+double precio;
+List<Edificio> lista= new List<Edificio>();
+
+do
+{
+    Console.Write("Ingrese el tipo de edificio: ");
+    tipo= Console.ReadLine();
+
+    Console.Write("Ingrese el valor del edificio: ");
+    precio = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Desea continuar? (S/N) o (s/n)");
+
+    opcion = Console.ReadLine();
+
+} while (opcion.ToUpper() !="N");
+
+
