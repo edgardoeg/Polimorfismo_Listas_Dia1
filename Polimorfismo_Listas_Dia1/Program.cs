01a0be03184b2ba56b29c55a65a7bf165d8ba404
edgardoeg <edgardoeg@yahoo.com>
2025-02-03 19:04:17 -0600

    cambios

--- Polimorfismo_Listas_Dia1/Program.cs
+++ Polimorfismo_Listas_Dia1/Program.cs
@@ -12,10 +12,25 @@ do
     Console.Write("Ingrese el valor del edificio: ");
     precio = Convert.ToDouble(Console.ReadLine());
 
+    Edificio edificio = new Edificio(tipo, precio);
+    lista.Add(edificio);
+
     Console.Write("Desea continuar? (S/N) o (s/n)");
 
     opcion = Console.ReadLine();
 
+    Console.WriteLine();
+
+    
+
 } while (opcion.ToUpper() !="N");
+
+foreach (Edificio edificio in lista)
+{
+
+    edificio.Vender();
+
+}
+
 
 
